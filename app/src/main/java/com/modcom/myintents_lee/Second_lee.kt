a3790c91d7ff5732bd2ae5d195180b92022e8063
leemutai <mutailee361@gmail.com>
2021-03-15 13:37:02 +0300

    app h

---
+++ app/src/main/java/com/modcom/myintents_lee/Second_lee.kt
@@ -0,0 +1,11 @@
+package com.modcom.myintents_lee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Second_lee : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_lee)
+    }
+}
